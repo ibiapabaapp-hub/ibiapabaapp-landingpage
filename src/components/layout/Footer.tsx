68098a048dd9beddf19a20fd17e9382da291293b
Manuel C. <manuelwn21@gmail.com>
2026-01-14 00:43:13 -0300

    style(Footer): adds logo and improve text redability

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,24 +1,25 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import logo_horizontal from '../../assets/logo/ib-full-dark.svg';
 
 export function Footer() {
 	return (
 		<footer className='w-full flex flex-col md:flex-row gap-6 p-6 bg-[#202220] items-center justify-between mt-16'>
 			<div id='logo' className='flex gap-4 items-center text-sm'>
 				<Image
-					src={'/ib-full.svg'}
+					src={logo_horizontal}
 					alt='IbiapabaApp logotype'
 					width={174.01}
 					height={28}
 				/>
-				<p className='max-w-48'>
+				<p className='max-w-48 dark:text-muted-foreground text-accent'>
 					Todos os direitos reservados. IbiapabaApp, 2026.
 				</p>
 			</div>
 
 			<div
 				id='links'
-				className='text-muted-foreground flex flex-wrap gap-4 text-sm'
+				className='dark:text-muted-foreground text-accent flex flex-wrap gap-4 text-sm'
 			>
 				<Link
 					href='#header'
